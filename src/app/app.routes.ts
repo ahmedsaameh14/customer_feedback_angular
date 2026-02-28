49786ccc02ce5d1306ded6b58174d17a1d5cbee5
Ahmed Sameh <ahmedsaameh14@gmail.com>
2026-03-01 00:16:56 +0200

    Setup Project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '' , component: FeedbackComponent},
+    {path: 'dashboard' , component: DashboardComponent}
+];
