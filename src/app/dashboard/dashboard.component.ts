fcf09b9082cafec7ca40a3d38ada0d55385684d3
Ahmed Sameh <ahmedsaameh14@gmail.com>
2026-03-03 01:14:21 +0200

    Try to fix repitation data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,16 +7,16 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule , RouterLink , FormsModule],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent implements OnInit {
-feedbacks: any[] = [];
+  feedbacks: any[] = [];
   currentPage = 1;
   totalPages = 1;
   pageSize = 5;
-  
+
   // 1. Add search variable
   searchLanguage: string = '';
 
@@ -28,13 +28,19 @@ feedbacks: any[] = [];
 
   getFeedbacks(page: number = 1) {
     // 2. Pass search term to service
-    this._feedbackS.loadAllFeedbacks(page, this.pageSize, this.searchLanguage).subscribe({
-      next: (res: FeedbackRes) => {
-        this.feedbacks = res.result;
-        this.currentPage = res.page;
-        this.totalPages = res.totalPages;
-      }
-    });
+    this._feedbackS
+      .loadAllFeedbacks(page, this.pageSize, this.searchLanguage)
+      .subscribe({
+        next: (res: FeedbackRes) => {
+          this.feedbacks = res.result;
+          this.currentPage = res.page;
+          this.totalPages = res.totalPages;
+        },
+      });
+  }
+
+  trackById(index: number, item: any) {
+    return item._id; // استخدم الـ ID الحقيقي من قاعدة البيانات
   }
 
   // 3. Add search method
